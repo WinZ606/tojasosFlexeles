a729751b14a8a33beb1e4fe315b0a9a983a16a88
Dezso Vince <2005bnm@gmail.com>
2025-02-14 14:05:21 +0100

    dolgozzzas

--- main.js
+++ main.js
@@ -3,7 +3,14 @@ import { tojasok } from "./adat.js";
 let txt = "";
 
 for (let index = 0; index < tojasok.length; index++) {
-  if (index == 3) {
+  if (index == 4) {
+    txt += `<div class = "tojas4">
+      <h2>${tojasok[index].nev}</h2>
+      <p>${tojasok[index].szin}</p>
+      <p>${tojasok[index].meret}</p>
+      <img src=${tojasok[index].eleresiUt}>
+      </div>`;
+  } else if (index == 3) {
     txt += `<div class = "tojas3">
     <h2>${tojasok[index].nev}</h2>
     <p>${tojasok[index].szin}</p>
